cc1b8f85bf1294fbaa021001766bab82bc31eda3
oyebanjiyusuf3 <oyebanjiyusuf3@gmail.com>
2022-12-03 00:54:08 +0000

    fix issues: svg class

--- svg.class.php
+++ svg.class.php
@@ -9,7 +9,7 @@
 *
 * ============================================================================
 * 
-* Copyright (C) 
+* Copyright (C) 2022
 * 
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
