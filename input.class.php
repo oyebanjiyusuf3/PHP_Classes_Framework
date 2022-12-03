ffbf2c08423fc2ca501190c4de71cafe83c75631
oyebanjiyusuf3 <oyebanjiyusuf3@gmail.com>
2022-12-03 22:07:05 +0000

    fix issues: imput class

--- input.class.php
+++ input.class.php
@@ -11,7 +11,7 @@
 *
 * ============================================================================
 * 
-* Copyright (C) Y
+* Copyright (C) Yu
 * 
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
